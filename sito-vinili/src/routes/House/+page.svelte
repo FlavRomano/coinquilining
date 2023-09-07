05136658f4cce54b0ce6ae6694aa96185b4a075c
FlavRomano <flavio.romano92@gmail.com>
2023-09-07 20:00:55 +0200

    add skeleton

---
+++ sito-vinili/src/routes/House/+page.svelte
@@ -0,0 +1,5 @@
+<h2>Cleaning rotations</h2>
+
+<h2>Events calendar</h2>
+
+<h2>Outgoings</h2>
---
+++ sito-vinili/src/routes/Shopping/+page.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+  let addingToCart = false;
+  let addingToShopList = false;
+</script>
+
+<h2>Shopping List</h2>
+<button on:click={() => (addingToShopList = !addingToShopList)}>
+  {addingToShopList ? "Hide" : "Add to shopping list"}
+</button>
+
+<div>
+  {#if addingToShopList}
+    <form>
+      <label for="item">Item:</label>
+      <input type="text" name="item" id="item" />
+      <br />
+      <label for="description">Description:</label>
+      <input type="text" name="description" id="description" />
+      <br />
+      <br />
+      <input type="submit" value="Conferma" />
+    </form>
+  {:else}
+    <table>
+      <tr>
+        <th>Roommate</th>
+        <th>Item</th>
+        <th>Description</th>
+      </tr>
+      <tr>
+        <td>Flavio</td>
+        <td>Balsamo</td>
+        <td>Il più economico</td>
+      </tr>
+      <tr>
+        <td>Gabriele</td>
+        <td>Dentifricio</td>
+        <td>Colgate o Parodontax</td>
+      </tr>
+    </table>
+  {/if}
+</div>
+
+<h2>Cart</h2>
+<ul>
+  <li>Totale:</li>
+  <li>Flavio:</li>
+  <li>Gabriele:</li>
+</ul>
+<button on:click={() => (addingToCart = !addingToCart)}>
+  {addingToCart ? "Hide" : "Add to cart"}
+</button>
+
+<div>
+  {#if addingToCart}
+    <p>
+      TODO: In "Item" suggerisci gli elementi già presenti nella "Shopping list"
+    </p>
+    <form>
+      <label for="item">Item:</label>
+      <input type="text" name="item" id="item" />
+      <br />
+      <label for="price">Price:</label>
+      <input type="text" name="price" id="price" />
+      <br />
+      <br />
+      <input type="submit" value="Conferma" />
+    </form>
+  {:else}
+    <ul>
+      Flavio
+      <li>
+        <input type="checkbox" name="cartItem" id="check1" />
+        <label for="check1">Balsamo</label>
+        <input
+          type="text"
+          name="cartItem"
+          id="check1"
+          placeholder="Insert price"
+        />
+      </li>
+    </ul>
+    <ul>
+      Gabriele
+      <li>
+        <input type="checkbox" name="asd" id="check2" />
+        <label for="check2">Dentifricio</label>
+        <input
+          type="text"
+          name="cartItem"
+          id="check2"
+          placeholder="Insert price"
+        />
+      </li>
+    </ul>
+  {/if}
+</div>
