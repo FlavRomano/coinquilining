3ada1e4e66ece7fd441b6a0d851b02a1af1a0d31
FlavRomano <flavio.romano92@gmail.com>
2023-09-09 17:39:38 +0200

    add home

--- sito-vinili/src/routes/+page.svelte
+++ sito-vinili/src/routes/+page.svelte
@@ -1,1 +1,40 @@
-<h2>Quick actions</h2>
+<div
+  class="pt-2 grid gap-4 place-items-center
+lg:grid-cols-3 lg:grid-flow-row-dense lg:pl-4 lg:pr-4"
+>
+  <div
+    tabindex="-1"
+    class="w-4/5 collapse collapse-open border border-base-300 bg-primary text-primary-content shadow-xl
+    lg:col-span-2 lg:w-full lg:-mt-2"
+  >
+    <div class="collapse-title text-xl font-medium">Overview</div>
+    <div class="collapse-content">
+      <ul>
+        <li>- boh</li>
+        <li>- boh</li>
+        <li>- boh</li>
+        <li>- boh</li>
+        <li>- boh</li>
+        <li>- boh</li>
+        <li>- boh</li>
+        <li>- boh</li>
+        <li>- boh</li>
+      </ul>
+    </div>
+  </div>
+  <div
+    tabindex="-1"
+    class="w-4/5 collapse collapse-open border border-base-300 bg-secondary text-secondary-content shadow-xl
+    lg:w-full lg:-mt-32"
+  >
+    <div class="collapse-title text-xl font-medium">Notifications</div>
+    <div class="collapse-content">
+      <ul>
+        <li>+ asd</li>
+        <li>+ asd</li>
+        <li>+ asd</li>
+        <li>+ asd</li>
+      </ul>
+    </div>
+  </div>
+</div>
