95ead8f3a1ad2df53e05d1bce4ba3e9eea443e3a
FlavRomano <flavio.romano92@gmail.com>
2023-09-18 19:44:33 +0200

    sync

--- coinquilining/src/components/SignupModal.svelte
+++ coinquilining/src/components/SignupModal.svelte
@@ -10,18 +10,25 @@
 </script>
 
 <div class="dropdown dropdown-end">
+	<!-- svelte-ignore a11y-label-has-associated-control -->
 	<label tabindex="-1" class="btn m-1">Account</label>
 	<ul
 		tabindex="-1"
 		class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
 	>
 		<li><a on:click={showModalSignup}>Settings</a></li>
-		<li><a href="/">Quit</a></li>
+		<li><a href="/signout">Quit</a></li>
 	</ul>
 </div>
 
 <dialog id="my_modal_2" class="modal">
-	<div class="modal-box" />
+	<div class="modal-box">
+		<ul>
+			<li>Option</li>
+			<li>Option</li>
+			<li>Option</li>
+		</ul>
+	</div>
 	<form method="dialog" class="modal-backdrop">
 		<button>close</button>
 	</form>
