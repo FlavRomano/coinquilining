a39997bc04bcb1df3e0569e7c550833a21080baf
FlavRomano <flavio.romano92@gmail.com>
2023-09-20 19:28:11 +0200

    add house schema

--- coinquilining/src/lib/schemas.ts
+++ coinquilining/src/lib/schemas.ts
@@ -30,3 +30,12 @@ export const loginSchema = z.object({
 	email: z.string().email(),
 	password: z.string().min(6),
 });
+
+export const house = z.object({
+	name: z.string().min(3),
+	mates: z.array(
+		z.object({
+			email: z.string().email(),
+		})
+	),
+});
