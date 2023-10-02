fe0e0d3c97a89cb0a37ef0d4d43f198ec2114e40
FlavRomano <flavio.romano92@gmail.com>
2023-10-02 18:49:37 +0200

    add password show/hide (to be fixed)

--- coinquilining/src/components/settings/profile/ResetPassword.svelte
+++ coinquilining/src/components/settings/profile/ResetPassword.svelte
@@ -1,33 +1,26 @@
 <script lang="ts">
-	export let firstname, lastname: string;
+	import InputPassword from "$components/InputPassword.svelte";
 </script>
 
 <div class="pt-12 prose prose-xl w-full grid lg:grid-cols-2">
 	<div class="lg:pt-12 lg:pl-52">
+		<!-- svelte-ignore a11y-label-has-associated-control -->
 		<form action="" class="join join-vertical gap-4">
 			<div class="join-item">
 				<label class="label">
 					<span class="label-text">Old Password</span>
 				</label>
-				<input
-					type="password"
-					placeholder="Password"
-					class="input input-bordered lg:w-80"
-				/>
+				<InputPassword />
 			</div>
 
 			<div class="join-item">
 				<label class="label">
 					<span class="label-text">New Password</span>
 				</label>
-				<input
-					type="text"
-					placeholder="Password"
-					class="input input-bordered lg:w-80"
-				/>
+				<InputPassword />
 			</div>
 
-			<button class="btn btn-primary lg:w-80">CHANGE</button>
+			<button class="btn btn-primary">CHANGE</button>
 		</form>
 	</div>
 </div>
