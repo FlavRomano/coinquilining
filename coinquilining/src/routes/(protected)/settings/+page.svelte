743ad576f3c383dd1ae6a9806ac032e93b063d55
FlavRomano <flavio.romano92@gmail.com>
2023-10-03 19:05:12 +0200

    add menu data flow

--- coinquilining/src/routes/(protected)/settings/+page.svelte
+++ coinquilining/src/routes/(protected)/settings/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import Menu from "$components/settings/Menu.svelte";
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
 </script>
 
-<Menu />
+<Menu {data} />
