7668330f2538755cc7058581c84cd5c7df97cbac
FlavRomano <flavio.romano92@gmail.com>
2023-10-04 18:48:04 +0200

    rename form

--- coinquilining/src/components/fridge/FridgeAddForm.svelte
+++ coinquilining/src/components/fridge/FridgeAddForm.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	export let fridge_form;
-	export let fridge_constraints;
+	export let fridgeAdd_form;
+	export let fridgeAdd_constraints;
 	export let roommates;
 </script>
 
@@ -13,7 +13,7 @@
 		<label class="label"
 			><span class="label-text">Select owner</span>
 			<select
-				bind:value={$fridge_form.owner}
+				bind:value={$fridgeAdd_form.owner}
 				class="select select-bordered w-full max-w-xs"
 				name="owner"
 			>
@@ -25,56 +25,56 @@
 		</label>
 
 		<input
-			bind:value={$fridge_form.food_name}
+			bind:value={$fridgeAdd_form.food_name}
 			class="input input-bordered"
 			type="text"
 			name="food_name"
 			placeholder="food_name"
-			{...$fridge_constraints.food_name}
+			{...$fridgeAdd_constraints.food_name}
 		/>
 		<label class="label">
 			<span class="label-text">Purchase date</span>
 			<input
-				bind:value={$fridge_form.purchased_on}
+				bind:value={$fridgeAdd_form.purchased_on}
 				class="input input-bordered"
 				type="date"
 				name="purchased_on"
 				placeholder="purchased_on"
-				{...$fridge_constraints.purchased_on}
+				{...$fridgeAdd_constraints.purchased_on}
 			/>
 		</label>
 
 		<label class="label">
 			<span class="label-text">Expiration date</span>
 			<input
-				bind:value={$fridge_form.expiration_on}
+				bind:value={$fridgeAdd_form.expiration_on}
 				class="input input-bordered"
 				type="date"
 				name="expiration_on"
 				placeholder="expiration_on"
-				{...$fridge_constraints.expiration_on}
+				{...$fridgeAdd_constraints.expiration_on}
 			/>
 		</label>
 
 		<input
-			bind:value={$fridge_form.kind}
+			bind:value={$fridgeAdd_form.kind}
 			class="input input-bordered"
 			type="text"
 			name="kind"
 			placeholder="kind"
-			{...$fridge_constraints.kind}
+			{...$fridgeAdd_constraints.kind}
 		/>
 		<label class="label">
 			<span class="label-text">Price</span>
 			<input
-				bind:value={$fridge_form.price}
+				bind:value={$fridgeAdd_form.price}
 				class="input input-bordered"
 				type="number"
 				step="0.01"
 				min="0.01"
 				name="price"
 				placeholder="price"
-				{...$fridge_constraints.price}
+				{...$fridgeAdd_constraints.price}
 			/>
 		</label>
 		<button class="btn btn-primary">ADD</button>
