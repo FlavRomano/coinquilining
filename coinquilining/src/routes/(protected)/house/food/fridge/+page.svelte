709d87829ceb6121cebf37965e88c3ff60cabd42
FlavRomano <flavio.romano92@gmail.com>
2023-10-04 18:50:25 +0200

    add table options, edit and delete forms

--- coinquilining/src/routes/(protected)/house/food/fridge/+page.svelte
+++ coinquilining/src/routes/(protected)/house/food/fridge/+page.svelte
@@ -1,60 +1,124 @@
 <script lang="ts">
 	import Table from "$components/Table.svelte";
-	import FridgeForm from "$components/fridge/FridgeForm.svelte";
-	import { fridgeSchema } from "$types/lib/schemas.js";
+	import FridgeAddForm from "$components/fridge/FridgeAddForm.svelte";
+	import FridgeEditForm from "$components/fridge/FridgeEditForm.svelte";
+	import {
+		fridgeAddSchema,
+		fridgeRemoveSchema,
+		fridgeEditSchema,
+	} from "$types/lib/schemas.js";
 	import { superForm } from "sveltekit-superforms/client";
-	import type { PageData } from "./$types";
+
 	import type { Food } from "$types/lib/server/db/types";
+	import { writable } from "svelte/store";
+	import type { PageData } from "./$types";
 
 	export let data: PageData;
 	const table: Food[] = data.table;
 	const roommates: { firstname: string; lastname: string }[] = data.roommates;
-	const {
-		form: fridge_form,
-		errors: fridge_errors,
-		constraints: fridge_constraints,
-		message: fridge_message,
-		enhance: fridge_enhance,
-		capture: fridge_capture,
-		restore: fridge_restore,
-	} = superForm(data.insertFoodForm, {
-		taintedMessage: null,
-		validators: fridgeSchema,
-	});
 
-	$: if ($fridge_form.price && typeof $fridge_form.price === "string") {
-		$fridge_form.price = parseFloat($fridge_form.price);
-	}
+	const { form: fridgeAdd_form, constraints: fridgeAdd_constraints } =
+		superForm(data.insertFoodForm, {
+			taintedMessage: null,
+			validators: fridgeAddSchema,
+		});
+
+	const { form: fridgeRemove_form, constraints: fridgeRemove_constraints } =
+		superForm(data.deleteFoodForm, {
+			taintedMessage: null,
+			validators: fridgeRemoveSchema,
+		});
+
+	const { form: fridgeEdit_form, constraints: fridgeEdit_constraints } =
+		superForm(data.editFoodForm, {
+			taintedMessage: null,
+			validators: fridgeEditSchema,
+		});
+
+	let option = writable({
+		remove: false,
+		edit: false,
+		targets: {
+			deleteTargetID: (targetRowID) => {
+				$fridgeRemove_form.id = targetRowID;
+			},
+			editTarget: (targetRowFood) => {
+				$fridgeEdit_form.id = targetRowFood[0];
+				$fridgeEdit_form.owner = targetRowFood[1];
+				$fridgeEdit_form.food_name = targetRowFood[2];
+				$fridgeEdit_form.kind = targetRowFood[3];
+				$fridgeEdit_form.purchased_on = targetRowFood[4];
+				$fridgeEdit_form.expiration_on = targetRowFood[5];
+				$fridgeEdit_form.price = targetRowFood[6];
+			},
+		},
+		restore: () => {
+			$option.remove = false;
+			$option.edit = false;
+			$fridgeRemove_form.id = undefined;
+		},
+	});
 </script>
 
 <div class="flex flex-col items-center pb-4">
-	<div class="join join-horizontal gap-2">
+	<div class="join join-horizontal gap-1">
 		<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 		<button
-			class="btn lg:btn-wide btn-lg btn-outline btn-primary"
-			on:click={() => document.getElementById("addModal").showModal()}
-			>ADD</button
+			class="btn lg:btn-wide btn-lg btn-outline btn-primary join-item"
+			on:click={() => {
+				$option.restore();
+				document.getElementById("addModal").showModal();
+			}}>ADD</button
 		>
-		<div class="">
-			<dialog id="addModal" class="modal">
-				<div class="modal-box">
-					<FridgeForm
-						{fridge_form}
-						{fridge_constraints}
-						{roommates}
-					/>
-				</div>
-				<form method="dialog" class="modal-backdrop">
-					<button>close</button>
-				</form>
-			</dialog>
-		</div>
-		<button class="btn lg:btn-wide btn-lg btn-outline btn-error join-item"
-			>Remove</button
+		<dialog id="addModal" class="modal">
+			<div class="modal-box">
+				<FridgeAddForm
+					{fridgeAdd_form}
+					{fridgeAdd_constraints}
+					{roommates}
+				/>
+			</div>
+			<form method="dialog" class="modal-backdrop">
+				<button>close</button>
+			</form>
+		</dialog>
+
+		<button
+			class="btn lg:btn-wide btn-lg btn-outline btn-error join-item"
+			on:click={() => {
+				if (!$option.remove) $option.restore();
+				$option.remove = !$option.remove;
+			}}>Remove</button
 		>
-		<button class="btn lg:btn-wide btn-lg btn-outline btn-neutral join-item"
-			>Edit</button
+		<form action="fridge?/delete" id="remove" method="post">
+			<input
+				bind:value={$fridgeRemove_form.id}
+				type="text"
+				name="id"
+				hidden
+				{...$fridgeRemove_constraints.id}
+			/>
+		</form>
+
+		<button
+			class="btn lg:btn-wide btn-lg btn-outline btn-neutral join-item"
+			on:click={() => {
+				if (!$option.edit) $option.restore();
+				$option.edit = !$option.edit;
+			}}>Edit</button
 		>
+		<dialog id="editModal" class="modal">
+			<div class="modal-box">
+				<FridgeEditForm
+					{fridgeEdit_form}
+					{fridgeEdit_constraints}
+					{roommates}
+				/>
+			</div>
+			<form method="dialog" class="modal-backdrop">
+				<button>close</button>
+			</form>
+		</dialog>
 	</div>
 </div>
-<Table {table} />
+<Table {table} {option} />
