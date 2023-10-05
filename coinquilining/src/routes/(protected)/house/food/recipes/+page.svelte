38c958737cd0518c63128e770e4dfbd82a03bd23
FlavRomano <flavio.romano92@gmail.com>
2023-10-05 18:26:36 +0200

    add todo

---
+++ coinquilining/src/routes/(protected)/house/food/recipes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Table from "$components/Table.svelte";
+</script>
+
+<div class="flex flex-col place-items-center prose">
+	TODO
+	<ol class="list-decimal">
+		<li>Tabella che mergia Fridge e Pantry (con row selezionabili)</li>
+		<li>
+			Concatenare i nomi dei cibi e.g "food1+food2" e fare una fetch a
+			GialloZafferano <br /> https://www.giallozafferano.it/ricerca-ricette/food1+food2/
+		</li>
+		<li>
+			Analizzare i risultati restituiti dalla fetch e capire come
+			renderizzarli sulla pagina
+		</li>
+	</ol>
+</div>
