ed3644c77f0998e2936216370be83a54eb392924
FlavRomano <flavio.romano92@gmail.com>
2023-10-12 12:12:12 +0200

    sync

--- coinquilining/src/routes/api/receipts/+server.ts
+++ coinquilining/src/routes/api/receipts/+server.ts
@@ -3,3 +3,7 @@ import { json } from "@sveltejs/kit";
 export async function POST({ request, locals }) {
 	return json("ok");
 }
+
+export async function GET({ request, locals }) {
+	return json("ALL THE receipts");
+}
