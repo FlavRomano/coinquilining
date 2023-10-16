726beb9d965088ef2164ca6c46e0d000745d99dc
FlavRomano <flavio.romano92@gmail.com>
2023-10-16 18:13:54 +0200

    refactored recipes

--- coinquilining/src/routes/(protected)/house/food/recipes/[foods]/[page]/+page.svelte
+++ coinquilining/src/routes/(protected)/house/food/recipes/[foods]/[page]/+page.svelte
@@ -2,13 +2,11 @@
 	import PaginationButton from "$components/food/recipes/PaginationButton.svelte";
 	import FoodCard from "$components/food/recipes/FoodCard.svelte";
 	import { writable } from "svelte/store";
-	import { page, updated } from "$app/stores";
-	import { onMount } from "svelte";
+	import { page } from "$app/stores";
 
 	let recipes = writable([]);
 	const queryFoods = $page.params.foods;
 	const currentPage = $page.params.page;
-	let loaded = false;
 
 	async function logRecipes() {
 		const response = await fetch("/api/recipes", {
@@ -19,15 +17,16 @@
 		});
 
 		$recipes = [...(await response.json())];
-		loaded = true;
 	}
-
-	onMount(() => {
-		logRecipes();
-	});
 </script>
 
-{#if loaded}
+{#await logRecipes()}
+	<div class="flex h-screen">
+		<div class="m-auto">
+			<span class="loading loading-spinner loading-lg" />
+		</div>
+	</div>
+{:then}
 	<div class="m-5">
 		<div class="flex flex-col place-items-center">
 			<a class="btn btn-neutral" href="/house/food/recipes"
@@ -45,10 +44,4 @@
 			<PaginationButton {queryFoods} {currentPage} />
 		</div>
 	</div>
-{:else}
-	<div class="flex h-screen">
-		<div class="m-auto">
-			<span class="loading loading-spinner loading-lg" />
-		</div>
-	</div>
-{/if}
+{/await}
