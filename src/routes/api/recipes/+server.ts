748766e6e7c5c4043e653b3cad8619dae0e233c6
FlavRomano <flavio.romano92@gmail.com>
2023-10-19 23:21:37 +0200

    fix cache on recipes

--- src/routes/api/recipes/+server.ts
+++ src/routes/api/recipes/+server.ts
@@ -15,48 +15,6 @@ export async function GET({ request }) {
 		recipes: { title: string; link: string; img: string }[];
 	}[] = [];
 
-	// TODO: fix cache
-	// cache
-	page.on("request", async (request) => {
-		const url = request.url();
-		if (cache[url] && cache[url].expires > Date.now()) {
-			await request.respond(cache[url]);
-			return;
-		}
-		request.continue();
-	});
-
-	// cache
-	page.on("response", async (response) => {
-		const url = response.url();
-		const headers = response.headers();
-		const cacheControl = headers["cache-control"] || "";
-		const maxAgeMatch = cacheControl.match(/max-age=(\d+)/);
-		const maxAge =
-			maxAgeMatch && maxAgeMatch.length > 1
-				? parseInt(maxAgeMatch[1], 10)
-				: 0;
-
-		if (maxAge) {
-			if (cache[url] && cache[url].expires > Date.now()) return;
-		}
-
-		let buffer;
-
-		try {
-			buffer = await response.buffer();
-		} catch (err) {
-			return;
-		}
-
-		cache[url] = {
-			status: response.status(),
-			headers: response.headers(),
-			body: buffer,
-			expires: Date.now() + maxAge * 1000,
-		};
-	});
-
 	await page.goto(
 		`https://www.giallozafferano.it/ricerca-ricette/page${currentPage}/${queryFoods}`,
 		{ waitUntil: "domcontentloaded" }
